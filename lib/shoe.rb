a4a98cc36ccba89b2b639c49b3a64589c8165157
william <masterpkpk@gmail.com>
2020-11-19 10:30:53 -0500

    commit tree a9048d5957de566128e3a26fb78dbdf6ad6de85f

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,18 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+  
+
   def initialize(brand)
     @brand = brand
+   #put brand in BRANDS
+   #if brand already in BRANDS do not put into BRANDS
+   
+   
+    if BRANDS.include?(brand) != true
+      BRANDS << brand
+    end
   end
 
   def cobble
@@ -11,4 +21,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  
 end
